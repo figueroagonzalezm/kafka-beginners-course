9f0000dd1c0d302552e7abec457ae1c170c899c8
Miguel Figueroa <figueroagonzalezm@gmail.com>
2022-07-21 14:27:16 -0500

    Initial commit

---
+++ kafka-basics/src/main/java/io/kafka/demos/ProducerDemoWithCallback.java
@@ -0,0 +1,54 @@
+package io.kafka.demos;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    public static final String DEMO_JAVA = "demo_java";
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer");
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 10 ; i++) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(DEMO_JAVA, "hello world "+i);
+
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception e) {
+                    if (e == null) {
+                        log.info("Received metadata/ \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    } else {
+                        log.error("Error while producing", e);
+                    }
+                }
+            });
+            //todo: Next code makes that messages goes to different partitions because it sleeps one second between each message.
+            // If commenting this code the messages will go to the same partition because of the StickyPartitioner
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        producer.flush();
+
+        producer.close();
+    }
+}
